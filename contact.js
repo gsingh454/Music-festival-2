ba3652c0594791ece637b11328c9795731e17476
gsingh454 <80239218+gsingh454@users.noreply.github.com>
2022-11-13 20:11:18 -0800

    initial commit

---
+++ contact.js
@@ -0,0 +1,31 @@
+
+//Cristian's API Key
+const firebaseConfig = {
+    apiKey: "AIzaSyDRQvRLYymMSigvVJUB4wUwR8ZUy1v8lHk",
+    authDomain: "login-edb56.firebaseapp.com",
+    projectId: "login-edb56",
+    databaseURL: "https://login-edb56-default-rtdb.firebaseio.com/",
+    storageBucket: "login-edb56.appspot.com",
+    messagingSenderId: "677404444347",
+    appId: "1:677404444347:web:973f087ee7133642ea8b9e"
+  };
+  
+  const app = firebase.initializeApp(firebaseConfig); 
+  const dbRef = firebase.database().ref(); 
+   
+  const detailsRef = dbRef.child('userdetails'); 
+  detailsRef.on("child_added", function(snapshot, prevChildKey) { 
+    var newPost = snapshot.val(); 
+  }); 
+  function send(){ 
+    var email = document.getElementById("name").value; 
+    var username = document.getElementById("username").value; 
+    var password = document.getElementById("message").value; 
+    
+    detailsRef.push().set({ 
+      name: email, 
+      username: username, 
+      message: password, 
+    }); 
+   
+  } 
